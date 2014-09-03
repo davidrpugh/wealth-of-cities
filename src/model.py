c892894cba8550e5b328b1894f37e9e39113b382
David R. Pugh <drobert.pugh@gmail.com>
2014-09-03 13:08:57 -0400

    Added some functions for computing real wages and gdp, and relative prices.

--- src/model.py
+++ src/model.py
@@ -1,8 +1,13 @@
 import sympy as sym
 
+
+# define parameters
+elasticity_substitution = sym.DeferredVector('theta')
+
 # define variables
 nominal_gdp = sym.DeferredVector('Y')
 price_level = sym.DeferredVector('P')
+nominal_wage = sym.DeferredVector('W')
 
 
 def real_gdp(i):
@@ -10,3 +15,11 @@ def real_gdp(i):
     return nominal_gdp[i] / price_level[i]
 
 
+def real_wage(i):
+    """Real wage in city i."""
+    return nominal_wage[i] / price_level[i]
+
+
+def relative_prices(i, j):
+    """Relative price level between cities i and j."""
+    return price_level[i] / price_level[j]
