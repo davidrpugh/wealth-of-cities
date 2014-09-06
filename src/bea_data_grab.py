918ba4c95269caa128cccbe600117df98763d6a9
David R. Pugh <drobert.pugh@gmail.com>
2014-09-06 12:23:56 -0400

    Moved global variables inside main.

--- src/bea_data_grab.py
+++ src/bea_data_grab.py
@@ -20,26 +20,6 @@ import pandas as pd
 import json
 import requests
 
-# to access the BEA data we need an API key
-MY_API_KEY = '6F508DBE-6979-4E85-A335-8DAE6187FC0D'
-
-# the BEA uniform reference identifier serves as the base URL
-BEA_BASE_URL = 'http://www.bea.gov/api/data?'
-
-# we are interested in grabbing the following data series:
-key_codes = ['POP_MI',     # Total MSA population
-             'GDP_MP',     # Nominal GDP
-             'RGDP_MP',    # Real GDP
-             'PCRGDP_MP',  # Per capita real GDP
-             'TPI_MI',     # Total personal income
-             'PCPI_MI',    # Per capita personal income
-             'DIR_MI',     # Dividends, interest, and rent
-             'PCTR_MI',    # Personal current transfer receipts
-             'WS_MI',      # Wage and salary dispursements
-             'SUPP_MI',    # Supplements to wages and salary
-             'PROP_MI',    # Proprietors income
-             ]
-
 
 def download_bea_data(base_url, api_key, key_codes, mesg=False):
     """Downloads data from the BEA data API."""
@@ -66,6 +46,9 @@ def download_bea_data(base_url, api_key, key_codes, mesg=False):
         tmp_data = pd.DataFrame(tmp_data)
         data_frames.append(tmp_data)
 
+        # close the connection?
+        tmp_response.connection.close()
+
         if mesg:
             print('Done grabbing the {} data!'.format(key_code))
         else:
@@ -77,20 +60,42 @@ def download_bea_data(base_url, api_key, key_codes, mesg=False):
     return combined_data
 
 
-def clean_bea_data(raw_bea_data):
-    """Cleans the raw BEA data."""
+def remove_duplicate_rows(raw_bea_data):
+    """Removes duplicate data entries."""
     check_for_dups = ['CL_UNIT', 'Code', 'DataValue', 'GeoFips', 'NoteRef',
                       'TimePeriod', 'UNIT_MULT']  # BEA has duplicate GeoNames!
     clean_data = raw_bea_data.drop_duplicates(subset=check_for_dups)
-
     return clean_data
 
 
 def main():
-    raw_data = download_bea_data(BEA_BASE_URL, MY_API_KEY, key_codes)
-    clean_data = clean_bea_data(raw_data)
+    # to access the BEA data we need an API key
+    MY_API_KEY = '6F508DBE-6979-4E85-A335-8DAE6187FC0D'
+
+    # the BEA uniform reference identifier serves as the base URL
+    BEA_BASE_URL = 'http://www.bea.gov/api/data?'
+
+    # we are interested in grabbing the following data series:
+    key_codes = ['POP_MI',     # Total MSA population
+                 'GDP_MP',     # Nominal GDP
+                 'RGDP_MP',    # Real GDP
+                 'PCRGDP_MP',  # Per capita real GDP
+                 'TPI_MI',     # Total personal income
+                 'PCPI_MI',    # Per capita personal income
+                 'DIR_MI',     # Dividends, interest, and rent
+                 'PCTR_MI',    # Personal current transfer receipts
+                 'WS_MI',      # Wage and salary dispursements
+                 'SUPP_MI',    # Supplements to wages and salary
+                 'PROP_MI',    # Proprietors income
+                 ]
+
+    raw_data = download_bea_data(BEA_BASE_URL, MY_API_KEY, key_codes, True)
+    clean_data = remove_duplicate_rows(raw_data)
+
+    # save to disk
     clean_data.to_csv('../data/raw_bea_metro_data.csv')
 
+    return clean_data
 
 if __name__ == '__main__':
-    main()
+    bea_metro_data = main()
