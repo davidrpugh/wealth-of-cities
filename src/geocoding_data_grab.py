31860619922f2c1359fab0a3c202ae3ed6dc7dd5
David R. Pugh <drobert.pugh@gmail.com>
2014-09-23 17:47:26 -0400

    Added helper functions for extract lat/lng coords and computing Haversine distance.

--- src/geocoding_data_grab.py
+++ src/geocoding_data_grab.py
@@ -1,27 +1,65 @@
 """
-Apparently I can increase our quota by registering a billing credit card. All
-I need to do is get our quota to 100,000 elements per day and we ar golden. We 
-only need to grab the data once!
+Grabs latitude and longitude data from Google Geocoding API.
+
+author : David R. Pugh
+date : 2014-09-23
 
 """
+import math
 import json
-import numpy as np
-import pandas as pd
 import requests
+
+import bea
 
 MY_API_KEY = 'AIzaSyDEq0MIyTYoks-3gjtjy95FE7hHzhnLi7g'
 GOOGLE_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+METRO_NAMES = bea.data_frame['GeoName'].unique()
 
-metro_names = ['San Francisco-Oakland, MSA']
+
+def _extract_latitude_coords(json):
+    """Extract the latitude coordinate from raw json data."""
+    return tmp_data['results'][0]['geometry']['location']['lat']
+
+
+def _extract_longitude_coords(json):
+    """Extract the latitude coordinate from raw json data."""
+    return tmp_data['results'][0]['geometry']['location']['lng']
+
 
 # this may be inefficient but avoids Google's API throttle!
-for h, city_h in enumerate(metro_names):
+geocodes = {}
+
+for h, city_h in enumerate(METRO_NAMES[1:]):
 
     tmp_query = {'address': city_h,
                  'key': MY_API_KEY,
                  }
-
     tmp_response = requests.get(url=GOOGLE_BASE_URL, params=tmp_query)
     tmp_data = json.loads(tmp_response.content)
 
+    if tmp_data['status'] == 'OK':
+        tmp_lat = _extract_latitude_coords(tmp_data)
+        tmp_lng = _extract_longitude_coords(tmp_data)
+        geocodes[city_h] = {'lat': tmp_lat, 'lng': tmp_lng}
+
+    else:
+        break
+
     print('Done with ' + city_h + '!')
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points on the earth
+    (specified in decimal degrees).
+
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    km = 6367 * c
+    return km
