6239a66d1e15b10aad98727a4c6288bcf9d3264d
David R. Pugh <drobert.pugh@gmail.com>
2014-09-23 23:31:32 -0400

    Refactored the get_geo_coords function.

--- src/geocodes.py
+++ src/geocodes.py
@@ -11,14 +11,14 @@ data = bea.data_frame[['GeoName', 'GeoFips']].drop_duplicates()
 geolocator = geopy.geocoders.GoogleV3(timeout=10)
 
 
-def get_geo_coords(places, geolocator):
+def get_geo_coords(data, geolocator):
     """
     Return a Pandas DataFrame storing the latitude and longitude coords.
 
     Parameters
     ----------
-    places : Series
-        Pandas Series containing place names.
+    data : DataFrame
+        Pandas DataFrame containing GeoName and GeoFips columns.
     geolocator : object
         GeoPy geolocator object used to fetch the latitude and longitude
         coordinates.
@@ -31,26 +31,28 @@ def get_geo_coords(places, geolocator):
 
     """
     geo_coords = {}
-    for i, city_i in enumerate(data['GeoName']):
+    for i, geo_name in enumerate(data['GeoName']):
         try:
             tmp_idx = data.iloc[i]['GeoFips']
-            tmp_loc = geolocator.geocode(city_i)
+            tmp_loc = geolocator.geocode(geo_name)
             geo_coords[tmp_idx] = {'lat': tmp_loc.latitude,
                                    'lng': tmp_loc.longitude}
         except AttributeError:
-            print("Can't find " + city_i + "!")
+            print("Can't find " + geo_name + "!")
 
     df = pd.DataFrame.from_dict(geo_coords, orient='index')
     return df
 
 
 # this loop should create two ndarrays storing measures of pair-wise physical distances
-N = len(geo_coords.keys())
+geo_coords = get_geo_coords(data, geolocator)
+
+N = geo_coords.shape[0]
 great_circle_distance = np.array(np.zeros((N, N)))
 vincenty_distance = np.array(np.zeros((N, N)))
 
-for i, city_i in enumerate(geo_coords.keys()):
-    for j, city_j in enumerate(geo_coords.keys()):
+for i, geo_fips_1 in enumerate(geo_coords):
+    for j, geo_fips_2 in enumerate(geo_coords):
         tmp_metric = great_circle(geo_coords[city_i], geo_coords[city_j])
         great_circle_distance[i, j] = tmp_metric.kilometers
 
