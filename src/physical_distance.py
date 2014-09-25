2953ccdff2a17d91da990092722f8eabc9767786
David R. Pugh <drobert.pugh@gmail.com>
2014-09-25 11:34:28 -0400

    physical_distance now uses master.csv data file.

--- src/physical_distance.py
+++ src/physical_distance.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 
 
-def compute_physical_distance(geo_coords):
+def compute_physical_distance(data):
     """
     Compute measures of physical distance given geographical coordinates for
     U.S. metropolitan statistical areas (MSAs).
 
     Parameters
     ----------
-    geo_coords : DataFrame (shape = (N, 2))
+    data : DataFrame (shape = (N, 2))
         Pandas DataFrame containing geographical coordinate data for MSAs.
 
     Returns
@@ -20,29 +20,30 @@ def compute_physical_distance(geo_coords):
         physical distance between MSAs.
 
     """
-    N = geo_coords.shape[0]
+    N = data.shape[0]
     great_circle_distance = np.array(np.zeros((N, N)))
     vincenty_distance = np.array(np.zeros((N, N)))
 
     # inefficient given that distance is symmetric!
-    for i, geo_fips_1 in enumerate(geo_coords.index):
-        for j, geo_fips_2 in enumerate(geo_coords.index):
-            tmp_geo_coords_1 = tuple(geo_coords.ix[geo_fips_1])
-            tmp_geo_coords_2 = tuple(geo_coords.ix[geo_fips_2])
+    for i, geo_fips_1 in enumerate(data.index):
+        for j, geo_fips_2 in enumerate(data.index):
+            tmp_coords_1 = tuple(data.ix[geo_fips_1])
+            tmp_coords_2 = tuple(data.ix[geo_fips_2])
 
-            tmp_metric = great_circle(tmp_geo_coords_1, tmp_geo_coords_2)
+            tmp_metric = great_circle(tmp_coords_1, tmp_coords_2)
             great_circle_distance[i, j] = tmp_metric.kilometers
 
-            tmp_metric = vincenty(tmp_geo_coords_1, tmp_geo_coords_2)
+            tmp_metric = vincenty(tmp_coords_1, tmp_coords_2)
             vincenty_distance[i, j] = tmp_metric.kilometers
 
     return great_circle_distance, vincenty_distance
 
 
 # load the csv file containing the geocoordinates
-geo_coords = pd.read_csv('../data/google/geocoordinates.csv', index_col=0)
+data = pd.read_csv('../data/master.csv', index_col='GeoFips')
 
 # compute the physical distance matrices
+geo_coords = data[['lat', 'lng']].drop_duplicates()
 physical_distance_matrices = compute_physical_distance(geo_coords)
 great_circle_distance, vincenty_distance = physical_distance_matrices
 
