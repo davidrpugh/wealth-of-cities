96d09600511fd3d0dd6ec9f00b2c8ee08ae8f9b6
David R. Pugh <drobert.pugh@gmail.com>
2014-09-25 08:36:22 -0400

    Refactored to improve files names.

--- src/bea.py
+++ src/bea.py
@@ -1,12 +1,4 @@
-"""
-Fetches relevant data on Metropolitan Statistical Areas (MSAs) from the Bureau
-of Economic Analysis (BEA) data API.
-
-@author : David R. Pugh
-@date : 2014-09-23
-
-"""
-import pybea
+import pandas as pd
 
 
 def create_new_variables(panel):
@@ -36,13 +28,6 @@ def dataframe_to_panel(df):
     return panel
 
 
-def drop_unused_cols(df):
-    """Remove unused columns from the BEA combined BEA DataFrame."""
-    # incorporate the unit multiplier before dropping UNIT_MULT!
-    df.loc[:, 'DataValue'] = df['DataValue'] * 10**df['UNIT_MULT']
-    return df.drop(['NoteRef', 'UNIT_MULT'], axis=1)
-
-
 def rescale_variables(panel):
     """Rescale variables to units more appropriate for numerical work."""
     # default BEA unit is USD, the natural unit is billions of USD
@@ -65,35 +50,10 @@ def rescale_variables(panel):
         else:
             pass
 
+# load raw BEA data and convert to a Panel object
+dataframe = pd.read_csv('../data/bea/raw_bea_metro_data.csv', index_col=0)
+panel = dataframe_to_panel(dataframe)
 
-# we are interested in the following variables...
-key_codes = ['POP_MI',     # Total MSA population
-             'GDP_MP',     # Nominal GDP
-             'RGDP_MP',    # Real GDP
-             'PCRGDP_MP',  # Per capita real GDP
-             'TPI_MI',     # Total personal income
-             'PCPI_MI',    # Per capita personal income
-             'DIR_MI',     # Dividends, interest, and rent
-             'PCTR_MI',    # Personal current transfer receipts
-             'WS_MI',      # Wage and salary dispursements
-             'SUPP_MI',    # Supplements to wages and salary
-             'PROP_MI',    # Proprietors income
-             ]
-
-# ...in the following years
-years = ['2000', '2005', '2010']
-
-# fectch the data from the BEA data api...
-raw_data_frame = pybea.get_data(DataSetName='RegionalData',
-                                KeyCodes=key_codes,
-                                GeoFips='MSA',
-                                Year=years)
-
-# ...clean it and the save a copy to disk!
-data_frame = drop_unused_cols(raw_data_frame)
-data_frame.to_csv('../data/bea/raw_bea_metro_data.csv')
-
-# Convert to a Panel object, rescale, and add additional variables (in place!)
-panel = dataframe_to_panel(data_frame)
+# rescale, and add additional variables (in place!)
 rescale_variables(panel)
 create_new_variables(panel)
