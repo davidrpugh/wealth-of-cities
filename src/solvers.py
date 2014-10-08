31530b1aad51041ddb66731930fe9ebc7b7ba354
David R. Pugh <drobert.pugh@gmail.com>
2014-10-08 17:23:03 -0400

    Fixed goofy mistake in computing cached values.

--- src/solvers.py
+++ src/solvers.py
@@ -1,5 +1,124 @@
 import numpy as np
 from scipy import optimize
+import sympy as sym
+
+# define parameters
+f, beta, phi, tau, theta = sym.var('f, beta, phi, tau, theta')
+
+# define variables
+L, M, P, W, Y = sym.var('L, M, P, W, Y')
+
+
+class InitialGuess(object):
+
+    modules = modules = [{'ImmutableMatrix': np.array}, "numpy"]
+
+    def __init__(self, solver):
+        """
+        Create an instance of the InitialGuess class.
+
+        Parameters
+        ----------
+        solver : solvers.Solver
+            An instance of the solvers.Solver class.
+
+        """
+        self.solver = solver
+
+        # initialize cached values
+        self.__numeric_gdp = None
+        self.__numeric_num_firms = None
+        self.__numeric_wage = None
+        self.__symbolic_solution = None
+
+    @property
+    def _args(self):
+        """
+        Tuple of arguments to pass to functions used for numeric evaluation of
+        symbolic solution.
+
+        :getter: Return the current arguments
+        :type: tuple
+        """
+        return (P, L, f, beta, phi, tau, theta)
+
+    @property
+    def _numeric_gdp(self):
+        if self.__numeric_gdp is None:
+            self._numeric_gdp = sym.lambdify(self._args,
+                                             self._symbolic_solution[Y],
+                                             self.modules)
+        return self._numeric_gdp
+
+    @property
+    def _numeric_wage(self):
+        if self.__numeric_wage is None:
+            self._numeric_wage = sym.lambdify(self._args,
+                                              self._symbolic_solution[W],
+                                              self.modules)
+        return self._numeric_wage
+
+    @property
+    def _numeric_num_firms(self):
+        if self.__numeric_num_firms is None:
+            self._numeric_num_firms = sym.lambdify(self._args,
+                                                   self._symbolic_solution[M],
+                                                   self.modules)
+        return self._numeric_num_firms
+
+    @property
+    def _symbolic_equations(self):
+        """
+        List of symbolic equations defining a model with a single city.
+
+        :getter: Return the current list of model equations.
+        :type: list
+
+        """
+        # define model equations using derivation from paper
+        eqn1 = ((1 / (theta - 1)) * ((theta / (theta - 1)) * (1 / phi))**-theta *
+                (W / P)**-theta * (Y / P) - phi * f)
+        eqn2 = (beta * L - (M / phi) * (theta / (theta - 1)) * ((theta / (theta - 1)) *
+                (1 / phi))**-theta * (W / P)**-theta * (Y / P))
+        eqn3 = Y - beta * L * W
+
+        return [eqn1, eqn2, eqn3]
+
+    @property
+    def _symbolic_solution(self):
+        """
+        Dictionary of symbolic expressions for analytic solution to the single
+        city model.
+
+        :getter: Return the analytic solution to the model as a dictionary.
+        :type: dict
+
+        """
+        if self.__symbolic_solution is None:
+            self.__symbolic_solution = sym.solve(self._symbolic_equations,
+                                                 Y, W, M, dict=True)
+        self.__symbolic_solution
+
+    @property
+    def guess(self):
+        """
+        The initial guess for the model equilibrium.
+
+        :getter: Return current initial guess.
+        :type: numpy.ndarray
+
+        """
+        # extract model data
+        num_cities = self.solver.model.N
+        params = self.solver.model.params
+        population = self.solver.model.population
+
+        P0 = np.repeat(1.0, num_cities-1)
+        Y0 = self._numeric_gdp(1.0, population[:num_cities], **params)
+        W0 = self._numeric_wage(1.0, population[:num_cities], **params)
+        M0 = self._numeric_num_firms(1.0, population[:num_cities], **params)
+
+        return np.hstack((P0, Y0, W0, M0))
 
 
 class Solver(object):
@@ -15,6 +134,9 @@ class Solver(object):
 
         """
         self.model = model
+
+        # create an instance of the InitialGuess class
+        self.initial_guess = InitialGuess(self)
 
     def system(self, X):
         """
@@ -62,7 +184,7 @@ class Solver(object):
 
         return jac
 
-    def solve(self, initial_guess, method, **kwargs):
+    def solve(self, method, **kwargs):
         """
         Solve the system of non-linear equations describing the equilibrium.
 
@@ -78,7 +200,7 @@ class Solver(object):
         """
         # solve for the model equilibrium
         result = optimize.root(self.system,
-                               x0=initial_guess,
+                               x0=self.initial_guess,
                                jac=self.jacobian,
                                method=method,
                                **kwargs
@@ -116,7 +238,7 @@ if __name__ == '__main__':
 
     initial_guess = get_initial_guess(N, **model.params)
 
-    result = solver.solve(initial_guess, method='hybr')
+    result = solver.solve(method='hybr', tol=1e-6)
 
     print("Solution converged? {}".format(result.success))
     print("Equilibrium nominal price levels:\n{}".format(result.x[:N-1]))
